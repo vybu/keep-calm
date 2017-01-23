0d1ae90d6f2f674da864b1ad98b847952da60de7
Vytenis <vytenis.butkevicius@gmail.com>
2017-01-23 23:37:12 +0200

    Move img to between text

--- src/main/components/History.jsx
+++ src/main/components/History.jsx
@@ -19,13 +19,6 @@ class History extends React.Component {
                 <p>Originally Keep Calm And Carry On posters were produced by British government in the dawn of WW2
                     (1939) as a way to boost morale of British people, who were threatened by massive air attack on
                     major cities. However not many of those posters were shown to public.</p>
-                <p>In year 2000 a copy of original poster was discovered and since then these posters rose in popularity
-                    and became known worldwide. Thus followed commercialization and companies started putting Keep Calm
-                    And Cary On on all kinds of products with different phrases and icons</p>
-                <p>Nowadays it is variations of these posters are used for everything. Usually top words "Keep Calm And"
-                    are kept in place, while lower part of the text, color, and icon are adjusted for the purpose.
-                    read more on <a href="https://en.wikipedia.org/wiki/Keep_Calm_and_Carry_On" target="_blank">Wikipedia</a>
-                </p>
                 <div className="History-imageWrap">
                     <img
                         src={originalImg}
@@ -36,6 +29,14 @@ class History extends React.Component {
                             target="_blank">Wikipedia</a>
                     </div>
                 </div>
+                <p>In year 2000 a copy of original poster was discovered and since then these posters rose in popularity
+                    and became known worldwide. Thus followed commercialization and companies started putting Keep Calm
+                    And Cary On on all kinds of products with different phrases and icons</p>
+                <p>Nowadays it is variations of these posters are used for everything. Usually top words "Keep Calm And"
+                    are kept in place, while lower part of the text, color, and icon are adjusted for the purpose.
+                    read more on <a href="https://en.wikipedia.org/wiki/Keep_Calm_and_Carry_On" target="_blank">Wikipedia</a>
+                </p>
+
             </PageBody>
         );
     }
