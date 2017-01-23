804af5631551ea0fc6c412991487b835add7e515
Vytenis <vytenis.butkevicius@gmail.com>
2017-01-23 22:24:38 +0200

    Improve history section.
    Add responsive img.

--- src/main/components/History.jsx
+++ src/main/components/History.jsx
@@ -6,11 +6,27 @@ const { PageBody } = pageStructure.components;
 class History extends React.Component {
     render() {
         return (
-            <PageBody>
+            <PageBody cls={'History'}>
                 <h1>Keep Calm And Carry On posters history</h1>
-                <p>Originally Keep Calm And Carry On posters were produced by British government in the dawn of WW2 (1939) as a way to boost morale of British people, who were threatened by massive air attack on major cities. However not many of those posters were shown to public.</p>
-                <p>In year 2000 a copy of original poster was discovered and since then these posters rose in popularity and became known worldwide. Thus followed commercialization and companies started putting Keep Calm And Cary On on all kinds of products with different phrases and icons</p>
-                <p>Nowadays it is variations of these posters are used for everything. Usually top words "Keep Calm And" are kept in place, while lower part of the text, color, and icon are adjusted for the purpose.</p>
+                <p>Originally Keep Calm And Carry On posters were produced by British government in the dawn of WW2
+                    (1939) as a way to boost morale of British people, who were threatened by massive air attack on
+                    major cities. However not many of those posters were shown to public.</p>
+                <p>In year 2000 a copy of original poster was discovered and since then these posters rose in popularity
+                    and became known worldwide. Thus followed commercialization and companies started putting Keep Calm
+                    And Cary On on all kinds of products with different phrases and icons</p>
+                <p>Nowadays it is variations of these posters are used for everything. Usually top words "Keep Calm And"
+                    are kept in place, while lower part of the text, color, and icon are adjusted for the purpose.
+                    read more on <a href="https://en.wikipedia.org/wiki/Keep_Calm_and_Carry_On" target="_blank">Wikipedia</a></p>
+                <div className="History-imageWrap">
+                    <img
+                        src={require('./../../vendor/Keep_Calm_And_Carry_On_-_Original_poster_-_Barter_Books_-_17-Oct-2011_1_tuqh7q.jpg')}
+                        alt="Keep_Calm_And_Carry_On_-_Original_poster_-_Barter_Books_-_17-Oct-2011"/>
+                    <div className="History-imageTitle"><span className="History-imageTitleSymbol">&#9830;</span>Original
+                        Poster from 1939 - <a
+                            href="https://en.wikipedia.org/wiki/File:Keep_Calm_And_Carry_On_-_Original_poster_-_Barter_Books_-_17-Oct-2011.jpg"
+                            target="_blank">Wikipedia</a>
+                    </div>
+                </div>
             </PageBody>
         );
     }
--- src/modules/pageStructure/components/PageBody.jsx
+++ src/modules/pageStructure/components/PageBody.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PageBody = props => (
-    <main className="PageBody">
+    <main className={`PageBody ${props.cls}`}>
         <div className="PageBody-topSpace"></div>
             {props.children}
         <div className="PageBody-bottomSpace"></div>
@@ -12,7 +12,12 @@ PageBody.propTypes = {
     children: React.PropTypes.oneOfType([
         React.PropTypes.arrayOf(React.PropTypes.node),
         React.PropTypes.node
-    ])
+    ]),
+    cls: React.PropTypes.string
+};
+
+PageBody.defaultProps = {
+    cls: ''
 };
 
 export default PageBody;
