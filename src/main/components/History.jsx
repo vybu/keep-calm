b34eec9248c8cdce2576c139a2b2d1989f6cf171
Vytenis <vytenis.butkevicius@gmail.com>
2017-01-23 08:51:14 +0200

    Add better text to history

--- src/main/components/History.jsx
+++ src/main/components/History.jsx
@@ -7,8 +7,10 @@ class History extends React.Component {
     render() {
         return (
             <PageBody>
-                <h1>Read here about keep calm history</h1>
-                <p>bla bla bla bla bla so much interesting info !</p>
+                <h1>Keep Calm And Carry On posters history</h1>
+                <p>Originally Keep Calm And Carry On posters were produced by British government in the dawn of WW2 (1939) as a way to boost morale of British people, who were threatened by massive air attack on major cities. However not many of those posters were shown to public.</p>
+                <p>In year 2000 a copy of original poster was discovered and since then these posters rose in popularity and became known worldwide. Thus followed commercialization and companies started putting Keep Calm And Cary On on all kinds of products with different phrases and icons</p>
+                <p>Nowadays it is variations of these posters are used for everything. Usually top words "Keep Calm And" are kept in place, while lower part of the text, color, and icon are adjusted for the purpose.</p>
             </PageBody>
         );
     }
