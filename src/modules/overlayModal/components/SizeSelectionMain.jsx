e712dbb03854201dd375dd9098a4939a61da3f16
Vytenis <vytenis.butkevicius@gmail.com>
2017-02-05 17:20:25 +0200

    Make modal size selection responsive and able to handle custom size

--- src/modules/overlayModal/components/SizeSelectionMain.jsx
+++ src/modules/overlayModal/components/SizeSelectionMain.jsx
@@ -45,11 +45,12 @@ const commonSizes = [
 ];
 
 
-const SizeSelectionItem = ({onClick, sizeItem}) => (
+const SizeSelectionItem = ({ onClick, sizeItem }) => (
     <div className="SizeSelection-item"
          onClick={onClick}>
         <h4 className="SizeSelection-itemName">{sizeItem.name}</h4>
-        <span className="SizeSelection-itemMeasurements">{`${parseInt(sizeItem.px.width)} x ${parseInt(sizeItem.px.height)}`}</span>
+        <span
+            className="SizeSelection-itemMeasurements">{`${parseInt(sizeItem.px.width)} x ${parseInt(sizeItem.px.height)}`}</span>
     </div>
 );
 
@@ -58,7 +59,7 @@ SizeSelectionItem.propTypes = {
     sizeItem: React.PropTypes.object
 };
 
-const CustomValuesItem = ({onClick}) => (
+const CustomValuesItem = ({ onClick }) => (
     <div className="SizeSelection-item"
          onClick={onClick}>
         <h4 className="SizeSelection-itemName">Custom</h4>
@@ -70,17 +71,106 @@ CustomValuesItem.propTypes = {
 };
 
 
-const SizeSelectionMain = ({ onSuccessfullSizeSelect, serviceToCallOnSelect }) => {
-    return (
-        <div className="SizeSelection">
-            <h3>Size</h3>
-            <div className="SizeSelection-sizes">
-                {commonSizes.map((s, i) => <SizeSelectionItem key={i} sizeItem={s} onClick={() => onSuccessfullSizeSelect(serviceToCallOnSelect, s.px)}/>)}
-                <CustomValuesItem/>
-            </div>
-        </div>
-    );
+const InitialMenu = ({ onSuccessfullSizeSelect, onCustomSelect }) => (
+    <div className="SizeSelection-sizes">
+        {commonSizes.map((s, i) => <SizeSelectionItem key={i}
+                                                      sizeItem={s}
+                                                      onClick={() => onSuccessfullSizeSelect(s.px)}/>)}
+        <CustomValuesItem onClick={onCustomSelect}/>
+    </div>
+);
+
+InitialMenu.propTypes = {
+    onSuccessfullSizeSelect: React.PropTypes.func,
+    onCustomSelect: React.PropTypes.func
 };
+
+
+class CustomSizeForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            width: 500,
+            height: 600
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleWidthChange = this.handleWidthChange.bind(this);
+        this.handleHeightChange = this.handleHeightChange.bind(this);
+        this.isValidInput = this.isValidInput.bind(this);
+    }
+
+    isValidInput() {
+        const { width, height } = this.state;
+        return width > 0 && width < 15000 && height > 0 && height < 150000;
+    }
+
+    handleSubmit(ev) {
+        ev.preventDefault();
+
+        if (this.isValidInput()) {
+            this.props.onSubmit(this.state);
+        }
+    }
+
+    handleWidthChange(ev) {
+        this.setState({ width: ev.target.value });
+    }
+
+    handleHeightChange(ev) {
+        this.setState({ height: ev.target.value });
+    }
+
+    render() {
+        return (
+            <form className="SizeSelection-form" onSubmit={this.handleSubmit}>
+                <div className="SizeSelection-formGroup">
+                    <label>Width (px)</label>
+                    <input ref={r => this.width = r} onClick={() => this.width.select()} value={this.state.width}
+                           onChange={this.handleWidthChange} type="number"/>
+                </div>
+                <div className="SizeSelection-formGroup">
+                    <label>Height (px)</label>
+                    <input ref={r => this.height = r} onClick={() => this.height.select()} value={this.state.height}
+                           onChange={this.handleHeightChange} type="number"/>
+                </div>
+
+                <button disabled={!this.isValidInput()}>Submit</button>
+            </form>
+        );
+    }
+}
+
+CustomSizeForm.propTypes = {
+    onSubmit: React.PropTypes.func
+};
+
+
+class SizeSelectionMain extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isCustom: false
+        };
+    }
+
+    render() {
+        const { onSuccessfullSizeSelect, serviceToCallOnSelect } = this.props;
+        const onSizeSubmit = (s) => onSuccessfullSizeSelect(serviceToCallOnSelect, s);
+        return (
+            <div className="SizeSelection">
+                <h3>Size</h3>
+                {this.state.isCustom ?
+                    <CustomSizeForm onSubmit={onSizeSubmit}/> :
+                    <InitialMenu onSuccessfullSizeSelect={onSizeSubmit}
+                                 onCustomSelect={() => this.setState({ isCustom: true })}/>
+                }
+            </div>
+        );
+    }
+}
 
 SizeSelectionMain.propTypes = {
     onSuccessfullSizeSelect: React.PropTypes.func,
