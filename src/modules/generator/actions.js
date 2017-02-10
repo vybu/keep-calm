181b308c0e41895739dec717f801a44f7de68bba
Vytenis <vytenis.butkevicius@gmail.com>
2017-02-10 10:05:34 +0200

    Latest touches

--- src/modules/generator/actions.js
+++ src/modules/generator/actions.js
@@ -8,3 +8,37 @@ export const generatePosters = (generationConfig) => (dispatch, getState) => {
 
     dispatch({ type: constants.ADD_GENERATED_POSTERS, configs: generatedConfigs });
 };
+
+
+
+/*
+
+* Resources:
+
+- Color manipulations and assertions: https://github.com/bgrins/TinyColor, https://github.com/Qix-/color, https://github.com/brehaut/color-js, https://github.com/gka/chroma.js
+- Get image color palette: https://github.com/lokesh/color-thief/
+
+* Rules:
+
+- Text and background provided minimum contrast. Or maybe mostly favor black / white text?
+
+
+
+* Possible user flows:
+
+** Initialization
+- We automatically generate posters and just show them in area below;
+- User has to click button to generate poster;
+** Generation
+- For each mood group we generate N banners and just show all groups
+- We simply show premade templates shuffle them for different users.
+
+TODO:
+1. Isskirti pagrindines "mood" kategorijas. (sad, funny, exciting, luxurious | serious | authoritative)
+2. Sukategorizuoti icon images.
+3. Sukategorizuoti spalvas arba padaryt spalvu dali visiskai automatine: reiktu spalvu vertinimo libo, spalvu generavimo logikos.
+4. Sukategorizuoti fontus.
+5. Pabandyti viska sujungti ir paziureti ar kategorizacija islieka
+6. Padaryti kazkokia vertinimo sistema ir duoti zmonems pavertinti, taip suzinant ar veikia
+*
+* */
--- src/modules/pageStructure/components/PageNav.jsx
+++ src/modules/pageStructure/components/PageNav.jsx
@@ -4,8 +4,9 @@ import { Link } from 'react-router';
 const PageNav = () => (
     <nav className="PageNav">
         <div className="PageNav-logo">
-            <span className="PageNav-logoIcon">&#0167;</span>
-            <span className="PageNav-logoText">KeepCalm.top</span>
+            <img style={{width: '200px'}} src={require('./KeepCalmLogo.png')} alt=""/>
+            {/*<span className="PageNav-logoIcon">&#0167;</span>*/}
+            {/*<span className="PageNav-logoText">KeepCalm.top</span>*/}
         </div>
         <div className="PageNav-controls">
             <Link activeClassName="is-active" className="PageNav-controlsItem" to={'/generator'}>Generator</Link>
